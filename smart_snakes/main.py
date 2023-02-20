e3c3e231420813e0719fcffa7c8172dd1cf8071c
Julian Schubert <informatik.schubert@gmail.com>
2023-02-20 23:52:31 +0100

    Added CLI pygame loop to play on the console

--- smart_snakes/main.py
+++ smart_snakes/main.py
@@ -2,7 +2,11 @@
 SmartSnakes - by Buckfae
 """
 
+import pygame
+
 from smart_snakes.game.board import Board
+from smart_snakes.game.directions.absolute_direction import AbsoluteDirection
+from smart_snakes.game.directions.relative_direction import RelativeDirection
 from smart_snakes.log.snake_logger import logger
 
 
@@ -10,10 +14,44 @@ def main():
     """Plays snake using the magical power of AI"""
     logger.info('Smart Snakes - by Buckfae')
 
-    sample_board = Board(5, 5, 2)
+    screen = pygame.display.set_mode([700, 900])
 
-    logger.info(f'Board size: {sample_board.get_size()}')
-    logger.info(f'Sample board:\n{sample_board}')
+    board = Board(7, 9, 3)
+
+    clock = pygame.time.Clock()
+
+    running = True
+    while running:
+
+        clock.tick(1)
+
+        logger.info(f'New Frame')
+
+        move_dir = RelativeDirection.AHEAD
+
+        for event in pygame.event.get():
+
+            if event.type == pygame.QUIT:
+                logger.info(f'Exiting!')
+                running = False
+
+            # Snake Movement
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_w:
+                    move_dir = AbsoluteDirection.UP
+                elif event.key == pygame.K_a:
+                    move_dir = AbsoluteDirection.LEFT
+                elif event.key == pygame.K_s:
+                    move_dir = AbsoluteDirection.DOWN
+                elif event.key == pygame.K_d:
+                    move_dir = AbsoluteDirection.RIGHT
+
+        logger.info(f'Move direction: {move_dir}')
+        e = board.move_snake(move_dir)
+        if not e.alive:
+            running = False
+
+    pygame.quit()
 
 
 if __name__ == '__main__':
